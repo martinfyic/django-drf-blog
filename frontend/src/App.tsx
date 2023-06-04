c6d8ecd74e3f34c11174ca847fbbe69726a4eabb
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-04 14:55:12 -0300

    feat: :sparkles: start frontend React Ts

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<h1 className='uppercase'>Blog - Frontend</h1>
+			<h2>React Js + Django REST Framework</h2>
+		</>
+	);
+}
+
+export default App;
