ce0795acdd3bcae09f4b4a09239b28bebf10d428
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-04 16:41:53 -0300

    style: :art: format code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,19 @@ import './App.css';
 function App() {
 	return (
 		<>
-			<h1 className='uppercase'>Blog - Frontend</h1>
-			<h2>React Js + Django REST Framework</h2>
+			<h1 className="mt-9 text-center text-3xl font-bold ">Blog - Frontend</h1>
+			<h2 className="mt-4 text-center text-2xl font-semibold">React Js + Django REST Framework</h2>
+			<p className="mt-6 text-center px-12">
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque porro distinctio totam et repellendus aut
+				deserunt sapiente delectus inventore odit animi quam ipsum recusandae, quia reprehenderit quae magni pariatur
+				culpa rem. Accusantium fuga sapiente nostrum sequi. Consectetur minus ducimus nostrum quo perspiciatis aliquam
+				perferendis quibusdam vero, laudantium, labore a deserunt molestiae, voluptatibus eligendi omnis iusto optio
+				magnam alias quisquam! Earum debitis nisi quae numquam explicabo architecto mollitia, tenetur temporibus
+				voluptatem beatae tempora repellat quod consequatur totam adipisci inventore ab? Quod voluptates voluptatibus
+				iste debitis minima dignissimos aspernatur iure et distinctio maiores placeat voluptatum, officia natus laborum
+				incidunt commodi deleniti est iusto consequatur rem aperiam expedita tempore? Velit hic aliquam quam, inventore
+				molestias unde similique, omnis quasi alias earum sunt cumque!
+			</p>
 		</>
 	);
 }
