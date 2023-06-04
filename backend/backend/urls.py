52d1fce9d021f119561c1040f1429c241f082a07
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-04 13:55:45 -0300

    perf: :zap: add user and blog into urlpatterns backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),
+    path('blog/', include('api.urls')),
+    path('user/', include('user.urls')),
 ]
