428e67b030cf3a9bf3f47bcb5a4d4619ded36996
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-08 19:50:04 -0300

    feat: :zap: blog interface

---
+++ frontend/src/blog/interfaces/blog.interface.ts
@@ -0,0 +1,12 @@
+export interface Blog {
+	author: number;
+	categories: number[];
+	content: string;
+	date_created: string;
+	date_updated: string;
+	feature: boolean;
+	id: number;
+	overview: string;
+	slug: string;
+	title: string;
+}
