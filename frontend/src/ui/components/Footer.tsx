77263d4abaaa85631676255252925285e7eca2d6
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-08 22:55:25 -0300

    feat: :lipstick: add Navbar.tsx and Footer.tsx src/ui/components

---
+++ frontend/src/ui/components/Footer.tsx
@@ -0,0 +1,3 @@
+export const Footer = () => {
+	return <h1 className="text-lime-700 text-3xl">Footer</h1>;
+};
---
+++ frontend/src/ui/components/Navbar.tsx
@@ -0,0 +1,3 @@
+export const Navbar = () => {
+	return <h1 className="text-lime-700 text-3xl">Navbar</h1>;
+};
