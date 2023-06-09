073dffeac1679df6430f3570924ba6f9c33450cf
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-08 22:54:35 -0300

    feat: :sparkles: add enpoint to BlogItem

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Navbar } from '../ui';
-import { HomePage } from '../blog';
+import { Navbar, Footer } from '../ui';
+import { HomePage, BlogItem } from '../blog';
 
-export const AppRoutes = () => {
+export const AppRoutes: React.FC = () => {
 	return (
 		<>
 			<BrowserRouter>
 				<Navbar />
 				<Routes>
 					<Route path="/" element={<HomePage />} />
+					<Route path="/blog/:slug" element={<BlogItem />} />
+					<Route path="*" element={'404 | Not found'} />
 				</Routes>
+				<Footer />
 			</BrowserRouter>
 		</>
 	);
