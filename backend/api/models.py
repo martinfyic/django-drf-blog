77f4947f4a4b4146ce51b0d50e36318ab292751f
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-09 20:12:33 -0300

    perf: :zap: includ thumbnail in Category model

--- backend/api/models.py
+++ backend/api/models.py
@@ -34,6 +34,7 @@ class Category(models.Model):
     title = models.CharField(max_length=255)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
+    thumbnail = models.URLField(null=True)
 
     def __str__(self):
         return self.title
