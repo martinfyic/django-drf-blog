bc59e5dce957697ebd720637aba5ab308b5aeeeb
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-08 22:53:17 -0300

    feat: :sparkles: add getOneBlog into blogs/helpers/axios.ts

--- frontend/src/blog/helpers/axios.ts
+++ frontend/src/blog/helpers/axios.ts
@@ -1,11 +1,22 @@
 import axios from 'axios';
-import { Blog } from '..';
+import { Blog } from '../interfaces';
 
 export const getBlogs = (url: string, setBlogs: React.Dispatch<React.SetStateAction<Blog[]>>) => {
 	axios
 		.get(url)
 		.then((response) => {
 			setBlogs(response.data);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+};
+
+export const getOneBlog = (url: string, setBlog: React.Dispatch<React.SetStateAction<Blog>>) => {
+	axios
+		.get(url)
+		.then((response) => {
+			setBlog(response.data);
 		})
 		.catch((error) => {
 			console.log(error);
