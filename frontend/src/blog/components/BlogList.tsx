53ea27c76be1cfb652266fee27ff7ec020e60eca
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-10 23:17:02 -0300

    refactor: :bug: refactor blogs.map

--- frontend/src/blog/components/BlogList.tsx
+++ frontend/src/blog/components/BlogList.tsx
@@ -5,19 +5,21 @@ export const BlogList: React.FC<BlogsProps> = ({ blogs }) => {
 	return (
 		<>
 			<article className="flex flex-wrap container mt-20 gap-6">
-				{blogs?.map((blog: Blog) => (
+				{blogs?.results?.map((blog: Blog) => (
 					<Link to={`/blog/${blog.slug}`} key={blog.slug} className="w-full p-4">
-						<div className="bg-zinc-700 rounded-lg shadow p-4 h-full max-w-5xl mx-auto">
+						<div className="bg-zinc-600 rounded-lg shadow p-4 h-full max-w-5xl mx-auto">
 							<h4 className="text-lg font-semibold mb-2">{blog.title}</h4>
 							<h5 className="mb-5">
 								<span>{blog.author}</span> | {blog.date_created}
 							</h5>
 							<p>
 								{blog.categories.map((t) => (
-									<strong className="mx-2">{t.title}</strong>
+									<strong key={t.id} className="mx-2">
+										{t.title}
+									</strong>
 								))}
 							</p>
-							<p>{blog.overview}</p>
+							<p className="truncate">{blog.overview}</p>
 						</div>
 					</Link>
 				))}
