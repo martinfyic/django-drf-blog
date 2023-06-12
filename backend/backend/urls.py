784275f538e099d5870cbcc49715a9545ac883af
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-11 21:24:49 -0300

    chore: :art: formate code

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
 schema_view = get_schema_view(
     openapi.Info(
         title="Django REST framework BLOG",
