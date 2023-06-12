89da6a9bbc8f2c7d2dd486c4c2586d0bcefb2bfa
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-11 21:23:44 -0300

    refactor: :zap: pagination

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -4,9 +4,9 @@ from rest_framework.response import Response
 
 class BlogPagination(PageNumberPagination):
     page_query_param = 'p'
-    page_size = 2
+    page_size = 10
     page_size_query_param = 'page_size'
-    max_page_size = 2
+    max_page_size = 10
 
     # Opcional: puedes personalizar la respuesta de paginación
     def get_paginated_response(self, data):
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,28 +1,16 @@
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
-from rest_framework.pagination import PageNumberPagination
+from django.shortcuts import get_object_or_404
+
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
 
 from .models import Blog
 from .serializers import BlogSerializer
 
-
-class BlogPagination(PageNumberPagination):
-    page_size = 10  # Número de elementos por página
-    # Parámetro opcional para cambiar el número de elementos por página
-    page_size_query_param = 'page_size'
-    max_page_size = 100  # Número máximo de elementos por página permitido
-
-    # Opcional: puedes personalizar la respuesta de paginación
-    def get_paginated_response(self, data):
-        return Response({
-            'next': self.get_next_link(),
-            'previous': self.get_previous_link(),
-            'count': self.page.paginator.count,
-            'results': data
-        })
+from .pagination import BlogPagination
 
 
 @swagger_auto_schema(
@@ -35,11 +23,14 @@ def getBlogs(request):
     """
     Vista para obtener todos los blogs.
     """
-    blog = Blog.objects.all()
-    paginator = BlogPagination()
-    paginated_blog = paginator.paginate_queryset(blog, request)
-    serializer = BlogSerializer(paginated_blog, many=True)
-    return paginator.get_paginated_response(serializer.data)
+    if Blog.objects.all().exists():
+        blog = Blog.objects.all()
+        paginator = BlogPagination()
+        paginated_blog = paginator.paginate_queryset(blog, request)
+        serializer = BlogSerializer(paginated_blog, many=True)
+        return paginator.get_paginated_response(serializer.data)
+    else:
+        return Response({'error': 'No posts found'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @swagger_auto_schema(
@@ -52,9 +43,10 @@ def getOneBlog(request, slug):
     """
     Vista para obtener un blog por su slug.
     """
-    blog = Blog.objects.get(slug=slug)
+    # blog = Blog.objects.get(slug=slug)
+    blog = get_object_or_404(Blog, slug=slug)
     serializer = BlogSerializer(blog, many=False)
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @swagger_auto_schema(
@@ -77,7 +69,7 @@ def postBlog(request):
     categories = data.get('categories', [])
     blog.categories.set(categories)
     serializer = BlogSerializer(blog, many=False)
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @swagger_auto_schema(
@@ -91,11 +83,11 @@ def putBlog(request, slug):
     Vista para actualizar un blog por su slug.
     """
     data = request.data
-    blog = Blog.objects.get(slug=slug)
+    blog = get_object_or_404(Blog, slug=slug)
     serializer = BlogSerializer(instance=blog, data=data)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @swagger_auto_schema(
@@ -108,6 +100,6 @@ def deleteBlog(request, slug):
     """
     Vista para eliminar un blog por su slug.
     """
-    blog = Blog.objects.get(slug=slug)
+    blog = get_object_or_404(Blog, slug=slug)
     blog.delete()
-    return Response('Blog Eliminado')
+    return Response('Blog Eliminado', status=status.HTTP_200_OK)
